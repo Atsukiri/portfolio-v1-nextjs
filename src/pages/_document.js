12327a1efd40aec8dd23b10741ce1bede70458de
Iori-PC <milan.avorque@gmail.com>
2025-06-21 21:19:39 +0800

    initial i think

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Manrope:wght@400;700&family=Work+Sans:wght@400;700&family=DM+Sans:wght@400;600&family=Poppins:wght@400;700&family=Sora:wght@400;600&family=Space+Grotesk:wght@500;700&family=Bebas+Neue&family=Anton&family=Pacifico&family=Courgette&family=Amatic+SC:wght@400;700&family=Lilita+One&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
